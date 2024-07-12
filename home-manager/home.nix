6fda642b8eebde19a51d00bb873b0f7ffc30d978
Aaron VerDow <aaron@verdow.com>
2024-07-12 13:42:06 -0500

    add bash dircolors

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -114,9 +114,13 @@ in
   # Nicely reload system units when changing configs
   systemd.user.startServices = "sd-switch";
 
-  home.file.".bashrc".source = ./dotfiles/dot_bashrc;
+
   # hacky solution until I find a proper home for this, typically goes in /etc/ so root is impacted as well
   home.file.".bash.bashrc".source = ./dotfiles/dot_bash.bashrc;
+  # should be in /etc/DIR_COLORS ?
+  home.file.".dir_colors".source = ./dotfiles/dot_dir_colors;
+
+  home.file.".bashrc".source = ./dotfiles/dot_bashrc;
   home.file.".config/awesome/rc.lua".source = ./dotfiles/dot_config/awesome/rc.lua;
   home.file.".config/fusuma/config.yml".source = ./dotfiles/dot_config/fusuma/config.yml;
   home.file.".config/neofetch/config".source = ./dotfiles/dot_config/neofetch/config;
