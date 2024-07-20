2b7fd49d61fdcd385a08aa9f3d005a92b68e5048
Aaron VerDow <aaron@verdow.com>
2024-07-20 10:12:00 -0500

    host.nix

--- nixos/ipad/host.nix
+++ nixos/ipad/host.nix
@@ -1,1 +1,10 @@
+{ config, pkgs, ... }:    
+{
+  imports =
+    [
+      ./hardware-configuration.nix
+      ../configuration.nix
+    ];
+  networking.hostName = "ipad";
   services.xserver.videoDrivers = ["amdgpu"];
+}
