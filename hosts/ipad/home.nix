9008af08cd0b7c54d1685aeb62377c4061c922a9
Aaron VerDow <aaron@verdow.com>
2024-08-06 09:22:42 -0500

    add basic backlight script

--- hosts/ipad/home.nix
+++ hosts/ipad/home.nix
@@ -1,5 +1,14 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   imports = [ ../../common/home.nix ];
+  home.packages = with pkgs; [ 
+    # gui programs
+    (writeShellScriptBin "backlight" ''
+      set -euo pipefail
+      backlight="/sys/class/backlight/amdgpu_bl1"
+      max=$( cat $backlight/max_brightness )
+      echo $1 | awk -v max=$max '{printf "%.0f\n", ($1 / 100) * max}' | sudo tee $backlight/brightness
+    '')
+  ];
 }
