7162110cc27af69acf1af79943363540e3eac136
Aaron VerDow <aaron@verdow.com>
2024-09-29 12:40:15 -0500

    add docking scripts

--- hosts/ipad/home.nix
+++ hosts/ipad/home.nix
@@ -2,13 +2,23 @@
 
 {
   imports = [ ../../common/home.nix ];
-  home.packages = with pkgs; [ 
-    # gui programs
+  home.packages = with pkgs; [
     (writeShellScriptBin "backlight" ''
       set -euo pipefail
       backlight="/sys/class/backlight/amdgpu_bl1"
       max=$( cat $backlight/max_brightness )
       echo $1 | awk -v max=$max '{printf "%.0f\n", ($1 / 100) * max}' | sudo tee $backlight/brightness
     '')
+
+    (writeShellScriptBin "desk" ''
+      xrandr --output eDP --auto --below DisplayPort-1 --output DisplayPort-1 --mode 3440x1440
+      set_wallpaper
+    '')
+
+    (writeShellScriptBin "undock" ''
+      xrandr --output eDP --auto --output DisplayPort-1 --off
+      set_wallpaper
+    '')
+
   ];
 }
