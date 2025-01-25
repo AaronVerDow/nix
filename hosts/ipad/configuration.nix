0d60b5f88aace71756c3bc493f175876612522d0
Aaron VerDow <aaron@verdow.com>
2025-01-25 15:33:34 -0600

    log usb devices to use later

--- hosts/ipad/configuration.nix
+++ hosts/ipad/configuration.nix
@@ -8,6 +8,12 @@
   networking.hostName = "ipad";
   services.xserver.videoDrivers = ["amdgpu"];
 
+  # Primary desk flag
+  # 2109:2813
+
+  # Secondary flag
+  # 291a:a392
+
   home-manager = {
     extraSpecialArgs = { inherit inputs outputs; };
     users = {
