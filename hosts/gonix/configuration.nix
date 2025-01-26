22dc1d6b3e98e842c90e966e8a55964daab8c2f1
Aaron VerDow <aaron@verdow.com>
2025-01-26 15:47:05 -0600

    testing multiple kanata configs

--- hosts/gonix/configuration.nix
+++ hosts/gonix/configuration.nix
@@ -1,5 +1,6 @@
 { inputs, outputs, ... }:
 let
+  disableKeyboard = builtins.readFile ../../common/kanata/disable_keyboard.kbd;
   kanataConfig = builtins.readFile ../../common/kanata/fake_vim;
 in
 {
@@ -13,13 +14,17 @@ in
   services.kanata = {
     enable = true;
     keyboards = {
-      internalKeyboard = {
+      disable_keyboard = {
         devices = [
           # Replace the paths below with the appropriate device paths for your setup.
           # Use `ls /dev/input/by-path/` to find your keyboard devices.
           "/dev/input/by-path/pci-0000:00:14.0-usb-0:3:1.0-event-kbd"
           "/dev/input/by-path/pci-0000:00:14.0-usbv2-0:3:1.0-event-kbd"
         ];
+        extraDefCfg = "process-unmapped-keys yes";
+        config = disableKeyboard;
+      };
+      internalKeyboard = {
         extraDefCfg = "process-unmapped-keys yes";
         config = kanataConfig;
       };
