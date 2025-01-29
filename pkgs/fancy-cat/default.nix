905106cd55b380bd5b4a1c9e47d7039051b1142a
Aaron VerDow <aaron@verdow.com>
2025-01-28 18:37:41 -0600

    fancy-cat package

---
+++ pkgs/fancy-cat/default.nix
@@ -0,0 +1,66 @@
+{ 
+  stdenv,
+  lib,
+  fetchFromGitHub,
+  pkg-config,
+  mupdf,
+  harfbuzz,
+  freetype,
+  jbig2dec,
+  libjpeg,
+  openjpeg,
+  gumbo,
+  mujs,
+  zlib,
+  zig
+}:
+
+stdenv.mkDerivation {
+  pname = "fancy-cat";
+  version = "0.0.1";
+
+  src = fetchFromGitHub {
+    owner = "freref";
+    repo = "fancy-cat";
+    rev = "6a651ae9f3700c1b176734ddf1dd369d82cb6fbc";
+    hash = "sha256-E2WaXTn8+Ky9kdZxvQmEt63Ggo6Ns0fZ0Za+rQGIMSg=";
+  };
+
+  nativeBuildInputs = [ pkg-config ];
+
+  buildInputs = [
+    mupdf
+    harfbuzz
+    freetype
+    jbig2dec
+    libjpeg
+    openjpeg
+    gumbo
+    mujs
+    zlib
+    zig
+  ];
+
+  installPhase = ''
+    runHook preInstall
+
+    sed -i 's/mupdf-third/mupdf/g' build.zig
+
+    zig build --fetch
+    zig build --release=fast
+
+    mkdir -p $out/bin
+    cp -r ./zig-out/bin/fancy-cat $out/bin/
+
+    runHook postInstall
+  '';
+
+  meta = with lib; {
+    description = "PDF viewer for terminals using the Kitty image protocol";
+    homepage = "https://github.com/freref/fancy-cat";
+    license = licenses.mit;
+    maintainers = with maintainers; [ averdow ];
+    platforms = [ "x86_64-linux" ];
+  };
+
+}
