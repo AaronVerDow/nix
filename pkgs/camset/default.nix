15aed4bd2c2d0435262f5e8eb61a7b94919d4fcc
Aaron VerDow <aaron@verdow.com>
2025-02-09 15:39:48 -0600

    working camset build

--- pkgs/camset/default.nix
+++ pkgs/camset/default.nix
@@ -1,7 +1,14 @@
 {
   python312Packages,
   fetchFromGitHub,
-  pkgs
+  cairo,
+  glib,
+  gobject-introspection,
+  libGL,
+  stdenv,
+  v4l-utils,
+  gtk3,
+  wrapGAppsHook3
 }:
 
 python312Packages.buildPythonPackage rec {
@@ -12,9 +19,28 @@ python312Packages.buildPythonPackage rec {
     rev = "b813ba9b1d29f2d46fad268df67bf3615a324f3e";
     hash = "sha256-vTF3MJQi9fZZDlbEj5800H22GGWOte3+KZCpSnsSTaQ=";
   };
+
+  nativeBuildInputs = [
+    gobject-introspection
+    wrapGAppsHook3
+  ];
+
   propagatedBuildInputs = [ 
     python312Packages.pytest 
-    python312Packages.numpy 
-    pkgs.libsndfile 
+    python312Packages.pygobject3
+    python312Packages.opencv4
+    # python312Packages.numpy 
+    # pkgs.libsndfile 
+    cairo
+    glib
+    # gobject-introspection
+    libGL
+    stdenv.cc.cc.lib
+    v4l-utils
+    gtk3
   ];
+
+  preFixup = ''
+    makeWrapperArgs+=("''${gappsWrapperArgs[@]}")
+  '';
 }
