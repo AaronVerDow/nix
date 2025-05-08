1cf091d051ac6ad4c81b30e8fcaae837b277f3cc
Aaron VerDow <aaron@verdow.com>
2025-05-08 17:31:51 -0500

    add unhardwrap

--- common/dotfiles/dot_bashrc.sh
+++ common/dotfiles/dot_bashrc.sh
@@ -31,6 +31,7 @@ alias fd='fd --type f'
 alias ncdu='instead_use gdu'
 alias dmesg='sudo dmesg --color'
 alias ccat='command cat'
+alias unhardwrap='awk '\''BEGIN {RS="\n\n"; ORS="\n\n"} {gsub(/\n/, " "); print}'\'
 which fdfind &> /dev/null && alias fd='fdfind --type f' 
 
 # running vi against a directory will cd into it
