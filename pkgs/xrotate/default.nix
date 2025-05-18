ea0a6a21f0e0b89943eb6adb7323eabcfe80f9ed
Aaron VerDow <aaron@verdow.com>
2025-05-18 16:07:20 -0500

    update xrotate build to include manpage

--- pkgs/xrotate/default.nix
+++ pkgs/xrotate/default.nix
@@ -1,10 +1,31 @@
-{ pkgs ? import <nixpkgs> {} }:
+{ pkgs ? import <nixpkgs> { } }:
 
-let
-  scriptContent = builtins.readFile ./xrotate.sh;
-in
-pkgs.writeShellApplication {
-  name = "xrotate";
-  runtimeInputs = with pkgs; [ bash xorg.xrandr xorg.xinput awesome ];
-  text = scriptContent;
+pkgs.stdenv.mkDerivation {
+  pname = "xrotate";
+  version = "1.0.0";
+
+  src = ./.;
+
+  nativeBuildInputs = with pkgs; [ makeWrapper ];
+  buildInputs = with pkgs; [ bash xorg.xrandr xorg.xinput awesome ];
+
+  dontBuild = true;
+
+  installPhase = ''
+    mkdir -p $out/bin
+    mkdir -p $out/share/man/man1
+
+    cp $src/xrotate.sh $out/bin/xrotate
+    cp $src/man.1 $out/share/man/man1/xrotate.1
+
+    chmod +x $out/bin/xrotate
+    wrapProgram $out/bin/xrotate --prefix PATH : ${
+      pkgs.lib.makeBinPath [
+        pkgs.bash
+        pkgs.xorg.xrandr
+        pkgs.xorg.xinput
+        pkgs.awesome
+      ]
+    }
+  '';
 }
