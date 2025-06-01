6941b4fc9ef88e35a2ac82c555efcdae0ff9350b
Aaron VerDow <aaron@verdow.com>
2025-06-01 12:07:47 -0500

    update apparmor.d

--- pkgs/apparmor-d/default.nix
+++ pkgs/apparmor-d/default.nix
@@ -1,13 +1,13 @@
 { stdenv, fetchFromGitHub }:
 stdenv.mkDerivation rec {
   pname = "apparmor-d";
-  version = "unstable-2024-10-12";
+  version = "unstable-2025-05-27";
 
   src = fetchFromGitHub {
-    rev = "116272b8ada281178150f1c9a564aac1967121f6";
+    rev = "bf22a7786c39d3b56b87095bfd4479769b88ec1a";
     owner = "roddhjav";
     repo = "apparmor.d";
-    hash = "sha256-Yx9UJdmBqjMSPVwFyvidQXfQ4pdEKaDMfvi7gF6GSVc=";
+    hash = "sha256-J8h9LcZRxhTaZg7OU2m75upSoeD1i/abSCJQtX1WRsQ=";
   };
 
   doCheck = false;
