9933b97c8c29a1c15aa11429b38a9f8bf8af1db3
Aaron VerDow <aaron@verdow.com>
2025-07-24 15:16:44 -0500

    add selfhosted module

--- hosts/titanic/configuration.nix
+++ hosts/titanic/configuration.nix
@@ -5,6 +5,7 @@
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
       ../../common/configuration.nix
+      ./selfhosted/selfhosted.nix
     ];
 
   boot.supportedFilesystems = [ "zfs" ];
