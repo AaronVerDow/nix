38324e1696d475f9b4a4a276e8e069e01a0f34dc
Aaron VerDow <aaron@verdow.com>
2025-07-26 10:52:40 -0500

    add x home config to titanic

--- hosts/titanic/home.nix
+++ hosts/titanic/home.nix
@@ -1,7 +1,10 @@
 { ... }:
 
 {
-  imports = [ ../../common/home.nix ];
+  imports = [ 
+    ../../common/home.nix 
+    ../../common/x/home.nix 
+  ];
 
   home.sessionVariables = {
     OLLAMA_API_BASE = "http://127.0.0.1:11434";
