ace0555c9a29807bacd55fc877023fdca615f331
Aaron VerDow <aaron@verdow.com>
2025-08-06 08:59:17 -0500

    automatic modification of desktop files

--- pkgs/temp-nix-launcher/find_files.sh
+++ pkgs/temp-nix-launcher/find_files.sh
@@ -1,5 +1,20 @@
-set -euo pipefail
-set -x
+set -exuo pipefail
 program=$1
 
-find $( find /nix/store -maxdepth 1 | grep $program ) -type f | egrep '(share/applications|share/icon)'
+# automatically copy and modify desktop files and icons
+
+store=$( nix path-info "nixpkgs#$program" )
+tmp=$( mktemp -d )
+
+rsync -vrL "$store/share/icons" share/
+rsync -vrL "$store/share/applications" "$tmp"
+chmod -R +w share/ "$tmp"
+
+while read -r file; do
+    sed -i '/\[/d' "$file" # strip translations
+    sed -i 's/^Name=/Name=Nix Run /' "$file"
+    sed -i "s/^Exec=.*/Exec=temp-nix-launcher $program/" "$file"
+    sed -i '/TryExec/d' "$file"
+done < <( find "$tmp" -type f -name '*desktop' )
+
+rsync -vrL "$tmp/applications" share/
