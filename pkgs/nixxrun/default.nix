0baa786721ba01ef7805c6c44f260a4e8ef5e993
Aaron VerDow <aaron@verdow.com>
2025-08-07 14:55:38 -0500

    set default name, rename to launcher

--- pkgs/nixxrun/default.nix
+++ pkgs/nixxrun/default.nix
@@ -1,46 +1,61 @@
-{ pkgs ? import <nixpkgs> { } }:
+{
+  pkgs ? import <nixpkgs> { },
+}:
 
 let
-  desktopCollector = { name, buildInputs ? [ ], file_prefix ? "nxr_", name_prefix ? "Nix Run", ... } @ args:
-    pkgs.stdenv.mkDerivation ({
-      inherit name;
-      phases = ["installPhase"];
-      
-      buildInputs = buildInputs;
-      
-      installPhase = ''
-        set -x
-        tmp=$( mktemp -d )
-        for pkg in ${toString buildInputs}; do
-          # there has to be a better way
-          package_name=$( basename $pkg | cut -d- -f2- | sed 's/-[0-9].*//' )
-          mkdir -p $tmp/share
-          if [ -d "$pkg/share/icons" ]; then
-            ${pkgs.rsync}/bin/rsync -rL --chmod=Du+w $pkg/share/icons $tmp/share
-          fi
-          if [ -d "$pkg/share/applications" ]; then
-            ${pkgs.rsync}/bin/rsync -rL --chmod=Du+w $pkg/share/applications $tmp/share
-          fi
+  launcher =
+    {
+      name ? "nixxrun-launcher",
+      buildInputs ? [ ],
+      file_prefix ? "nxr_",
+      name_prefix ? "Nix Run",
+      ...
+    }@args:
+    pkgs.stdenv.mkDerivation (
+      {
+        name = if args ? name then args.name else name;
+        phases = [ "installPhase" ];
 
-          # add file_prefix to files to avoid collisions
-          find $tmp/share -type f | while read file; do
-            mv "$file" "$( dirname $file )/${file_prefix}$( basename $file )"
+        buildInputs = buildInputs;
+
+        installPhase = ''
+          tmp=$( mktemp -d )
+          for pkg in ${toString buildInputs}; do
+            # there has to be a better way
+            package_name=$( basename $pkg | cut -d- -f2- | sed 's/-[0-9].*//' )
+            mkdir -p $tmp/share
+            if [ -d "$pkg/share/icons" ]; then
+              ${pkgs.rsync}/bin/rsync -rL --chmod=Du+w $pkg/share/icons $tmp/share
+            fi
+            if [ -d "$pkg/share/applications" ]; then
+              ${pkgs.rsync}/bin/rsync -rL --chmod=Du+w $pkg/share/applications $tmp/share
+            fi
+
+            # add file_prefix to files to avoid collisions
+            find $tmp/share -type f | while read file; do
+              mv "$file" "$( dirname $file )/${file_prefix}$( basename $file )"
+            done
+
+            # modify desktop entries
+            find $tmp/share/applications -type f | while read file; do
+                sed -i 's/^Name=/Name=${name_prefix} /' "$file"
+                sed -i "s/^Exec=.*/Exec=nixxrun $package_name/" "$file"
+                sed -i "s/^Icon=/Icon=${file_prefix}/" "$file"
+                sed -i '/TryExec/d' "$file"
+            done
+
+            mkdir -p $out/share
+            ${pkgs.rsync}/bin/rsync -r $tmp/share/* $out/share
+            rm -r $tmp/share/*
           done
-
-          # modify desktop entries
-          find $tmp/share/applications -type f | while read file; do
-              sed -i 's/^Name=/Name=${name_prefix} /' "$file"
-              sed -i "s/^Exec=.*/Exec=nixxrun $package_name/" "$file"
-              sed -i "s/^Icon=/Icon=${file_prefix}/" "$file"
-              sed -i '/TryExec/d' "$file"
-          done
-
-          mkdir -p $out/share
-          ${pkgs.rsync}/bin/rsync -r $tmp/share/* $out/share
-          rm -r $tmp/share/*
-        done
-      '';
-    } // (removeAttrs args ["buildInputs" "file_prefix" "name_prefix"]));
+        '';
+      }
+      // (removeAttrs args [
+        "buildInputs"
+        "file_prefix"
+        "name_prefix"
+      ])
+    );
 in
 
 pkgs.stdenv.mkDerivation {
@@ -73,6 +88,6 @@ pkgs.stdenv.mkDerivation {
   '';
 
   passthru = {
-    desktopCollector = args: desktopCollector args;
+    launcher = args: launcher args;
   };
 }
