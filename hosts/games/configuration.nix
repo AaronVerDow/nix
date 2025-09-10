a6ad594e3f941612a72ca39eb3a211d343f37570
Aaron VerDow <aaron@verdow.com>
2025-09-10 18:41:31 -0500

    add virtualbox to games

--- hosts/games/configuration.nix
+++ hosts/games/configuration.nix
@@ -29,6 +29,10 @@
     };
   };
   hardware.xone.enable = true;
+  virtualisation.virtualbox.host.enable = true;
+  virtualisation.virtualbox.host.enableExtensionPack = true;
+  # virtualisation.virtualbox.host.enableKvm = true;
+  virtualisation.virtualbox.host.addNetworkInterface = false;
 
   services.kanata-config = {
     enable = true;
