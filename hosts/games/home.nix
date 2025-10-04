66912f9e7547b8af5311f3602f87488d53ebc5ef
Aaron VerDow <aaron@verdow.com>
2025-10-03 22:12:55 -0500

    -m add undock script

--- hosts/games/home.nix
+++ hosts/games/home.nix
@@ -10,7 +10,16 @@
     unstable.r2modman
     protonup-qt
     huestacean
+    (writeShellScriptBin "undock" ''
+      xrandr --output DP-1 --off
+      wallpaper_set
+    '')
+    (writeShellScriptBin "dual" ''
+      xrandr --output DP-1 --auto --pos 760x1440
+      wallpaper_set
+    '')
   ];
+  
   programs.lutris = {
     enable = true;
     winePackages = [
@@ -18,4 +27,21 @@
     ];
   };
 
+  xdg.desktopEntries = {
+    dual = {
+      name = "Dual Screen";
+      exec = "dual";
+      icon = "video-display";
+      type = "Application";
+    };
+  };
+
+  xdg.desktopEntries = {
+    undock = {
+      name = "Undock";
+      exec = "undock";
+      icon = "video-display";
+      type = "Application";
+    };
+  };
 }
