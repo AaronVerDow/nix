2f38fa3687975c0106f71123fdc6f30b12e4a47c
Aaron VerDow <aaron@verdow.com>
2026-03-08 14:53:16 -0500

    update hash

--- pkgs/openscad-preview.nvim/default.nix
+++ pkgs/openscad-preview.nvim/default.nix
@@ -6,7 +6,7 @@ vimUtils.buildVimPlugin {
     owner = "AaronVerDow";
     repo = "openscad-preview.nvim";
     rev = "0d6b6e45830e45c4e5f9831204ec12f57a250cbb";
-    hash = "sha256-ZCcy4fj7zVmjJmvFSgESir723NWq4ae1E7XAJ4ElRiI=";
+    hash = "sha256-OnlmYHt13Sx8VC8hHsL7a0dpzeJmNkWCkz/0YygC3LA=";
   };
   meta = with lib; {
     description = "Automatic OpenSCAD preview with Vim";
