f57dc7aed64341e78baa7f23fb88414f31c6edb5
Aaron VerDow <aaron@verdow.com>
2026-03-28 15:53:58 -0500

    fix agent variables

--- hosts/titanic/home.nix
+++ hosts/titanic/home.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ lib, pkgs, ... }:
 
 {
   imports = [ 
@@ -7,6 +7,12 @@
   ];
 
   home.sessionVariables = {
-    OLLAMA_API_BASE = "http://127.0.0.1:11434";
+    OPENAI_API_BASE = "http://127.0.0.1:11433/v1";
+    ANTHROPIC_BASE_URL = "http://127.0.0.1:11433";
   };
+
+  home.packages = lib.mkMerge [(with pkgs; [
+    unstable.stable-diffusion-cpp-cuda
+    ]) 
+  ];
 }
