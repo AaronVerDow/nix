267512586a04d390d74734f39d85641d8535e8a6
Aaron VerDow <aaron@verdow.com>
2026-05-25 13:54:49 -0500

    switch to neowall

--- common/x/home.nix
+++ common/x/home.nix
@@ -144,15 +144,7 @@ in
       '')
 
       (writeShellScriptBin "wallpaper_set" ''
-        killall esshader
-        # Get all connected outputs and spawn esshader on each
-        xrandr --query | grep connected | while read -r line; do
-          output=$(echo "$line" | awk '{print $1}')
-          dims=$(echo "$line" | grep -oP '\d+\sx\s\d+')
-          width=$(echo "$dims" | awk -F'x' '{print $1}')
-          height=$(echo "$dims" | awk -F'x' '{print $2}')
-          [ -n "$width" ] && nohup esshader --width $width --height $height --source ~/.config/wallpaper.glsl &>/dev/null &
-        done
+        	neowall
       '')
 
       (writeShellScriptBin "wallpaper_rotate" ''
