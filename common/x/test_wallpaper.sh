db1bc529a174d30ba5d7666afe4c6cc564ba4f31
Aaron VerDow <aaron@verdow.com>
2026-05-25 13:13:52 -0500

    launch multi esshader

--- common/x/test_wallpaper.sh
+++ common/x/test_wallpaper.sh
@@ -2,14 +2,12 @@
 set -euo pipefail
 set -x
 
-killall esshader
-
-nohup esshader --fullscreen --source ~/.config/wallpaper.glsl &>/dev/null &
+killall esshader || true
 
 # Get all connected outputs and spawn esshader on each
 xrandr --query | grep connected | while read -r line; do
   output=$(echo "$line" | awk '{print $1}')
-  dims=$(echo "$line" | grep -oP '\d+\sx\s\d+')
+  dims=$(echo "$line" | grep -oE '[0-9]+x[0-9]+')
   width=$(echo "$dims" | awk -F'x' '{print $1}')
   height=$(echo "$dims" | awk -F'x' '{print $2}')
   [ -n "$width" ] && nohup esshader --width $width --height $height --source ~/.config/wallpaper.glsl &>/dev/null &
