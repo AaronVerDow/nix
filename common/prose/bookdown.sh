07164158c027bd20b0f61d309babf77501d51a14
Aaron VerDow <aaron@verdow.com>
2026-06-01 16:58:59 -0500

    auto traverse bookdown

--- common/prose/bookdown.sh
+++ common/prose/bookdown.sh
@@ -1,10 +1,18 @@
+{
+while [ ! -f index.Rmd ] && [ -n "$(git rev-parse --show-toplevel 2>/dev/null)" ]; do
+  cd ..
+done
+
+if [ ! -f index.Rmd ]; then
+  echo "Error: index.Rmd not found" >&2
+  exit 1
+fi
+
 Rscript -e 'library(bookdown); render_book()'
 
-cmd=evince
-# cmd=fancy-cat
 pdf="$( pwd )/_book/_main.pdf"
-# html="$( pwd )/_book/index.html"
-
+cmd=evince
 if [ -f "$pdf" ]; then
-    pgrep -a $cmd | grep -q "$pdf" || $cmd "$pdf" &>> /dev/null &
+  pgrep -a $cmd | grep -q "$pdf" || $cmd "$pdf" &>> /dev/null &
 fi
+}
