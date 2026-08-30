044a55998bcba4b68261c00c845e8226d2d11f60
Aaron VerDow <aaron@verdow.com>
2026-08-30 09:34:16 -0500

    add kableExtra

--- common/prose/prose.nix
+++ common/prose/prose.nix
@@ -2,43 +2,45 @@
 { config, pkgs, ... }:
 with pkgs;
 let
-    wordCountContent = builtins.readFile ./wcgraph.sh;
-    bookdownContent = builtins.readFile ./bookdown.sh;
-    R-with-my-packages = rWrapper.override{ packages = with rPackages; [ 
-        bookdown
-        stringr
-        tidyverse
-        plantuml
-        plantuml-c4
-        plantuml-server
-    ]; };
+  wordCountContent = builtins.readFile ./wcgraph.sh;
+  bookdownContent = builtins.readFile ./bookdown.sh;
+  R-with-my-packages = rWrapper.override {
+    packages = with rPackages; [
+      bookdown
+      stringr
+      tidyverse
+      plantuml
+      plantuml-c4
+      plantuml-server
+      kableExtra
+    ];
+  };
 in
 {
-    home.packages = with pkgs; [
-        # mlterm # variable width terminal
-        R-with-my-packages
-        pandoc
-        texliveFull
-        # pandoc-mustache # broken due to future and python 3.13
-        languagetool
-        grammar
-        plantuml
-        # rWrapper.override{packages = [ rPackages.bookdown ];}
+  home.packages = with pkgs; [
+    # mlterm # variable width terminal
+    R-with-my-packages
+    pandoc
+    texliveFull
+    # pandoc-mustache # broken due to future and python 3.13
+    languagetool
+    grammar
+    plantuml
+    # rWrapper.override{packages = [ rPackages.bookdown ];}
 
+    (writeShellApplication {
+      name = "wcgraph";
+      runtimeInputs = with pkgs; [
+        python312Packages.termgraph
+        yq
+      ];
+      text = wordCountContent;
+    })
 
-        (writeShellApplication {
-            name = "wcgraph";
-            runtimeInputs = with pkgs; [ 
-                python312Packages.termgraph 
-                yq
-            ];
-            text = wordCountContent;
-        })
+    (writeShellApplication {
+      name = "bookdown";
+      text = bookdownContent;
+    })
 
-        (writeShellApplication {
-            name = "bookdown";
-            text = bookdownContent;
-        })
-        
-    ];
+  ];
 }
