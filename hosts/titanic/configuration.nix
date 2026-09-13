f31f61f91b23f0b5046cf975d96ae0b2635a5a91
Aaron VerDow <aaron@verdow.com>
2026-09-13 11:37:05 -0500

    make different ctx windows

--- hosts/titanic/configuration.nix
+++ hosts/titanic/configuration.nix
@@ -106,7 +106,19 @@
       in
       {
         models = {
-          "qwen3.8-27b" = {
+          "qwen3.8-27b-8k" = {
+            cmd = "${llama-server} --port \${PORT} -m /array/models/Qwen3.8-27B-UD-Q4_K_XL.gguf --temp 1.0 --top-p 0.95 --top-k 20 -c 8192 --no-webui";
+          };
+          "qwen3.8-27b-16k" = {
+            cmd = "${llama-server} --port \${PORT} -m /array/models/Qwen3.8-27B-UD-Q4_K_XL.gguf --temp 1.0 --top-p 0.95 --top-k 20 -c 16384 --no-webui";
+          };
+          "qwen3.8-27b-64k" = {
+            cmd = "${llama-server} --port \${PORT} -m /array/models/Qwen3.8-27B-UD-Q4_K_XL.gguf --temp 1.0 --top-p 0.95 --top-k 20 -c 65536 --no-webui";
+          };
+          "qwen3.8-27b-128k" = {
+            cmd = "${llama-server} --port \${PORT} -m /array/models/Qwen3.8-27B-UD-Q4_K_XL.gguf --temp 1.0 --top-p 0.95 --top-k 20 -c 131072 --no-webui";
+          };
+          "qwen3.8-27b-262k" = {
             cmd = "${llama-server} --port \${PORT} -m /array/models/Qwen3.8-27B-UD-Q4_K_XL.gguf --temp 1.0 --top-p 0.95 --top-k 20 -c 262144 --no-webui";
           };
         };
